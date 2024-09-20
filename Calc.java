f7c0969f20066ec819916aab2def7af9dfb12a67
Giulia Sousa <gm0414629@gmail.com>
2024-09-20 19:14:48 -0300

    Primeiro commit e criação dos métodos e do construtor

---
+++ Calc.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+
+public class Calc{
+    Scanner scan = new Scanner(System.in);
+    
+}
---
+++ Numero.java
@@ -0,0 +1,12 @@
+public class Numero {
+    private float valor;
+    public Numero(int valor){
+        this.valor=valor;
+    }
+    public void setValor(int valor){
+        this.valor=valor;
+    }
+    public float getValor(){
+        return this.valor;
+    }
+}
