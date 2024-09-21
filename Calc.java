a0be700b5f72a6904880d4a9484e6eab36f0d1f4
Giulia Sousa <gm0414629@gmail.com>
2024-09-21 19:05:37 -0300

    Implementação de tratativas de erro no switch

--- Calc.java
+++ Calc.java
@@ -7,18 +7,29 @@ public class Calc{
 
         Numero n1 = new Numero();
         Numero n2 = new Numero();
-        // Numero resultado = new Numero();
         String opc="S";
 
         while(opc.equalsIgnoreCase("s")){
             System.out.printf("%nDigite o 1° número: ");
-            n1.setValor(scan.nextInt());
+
+            while (!scan.hasNextDouble()) {
+                System.out.println("Digite somente números: ");
+                scan.next();
+            }
+            n1.setValor(scan.nextDouble());
 
             System.out.printf("%nDigite o operador (+, -, *, /): ");
             char operador= scan.next().charAt(0);
 
             System.out.printf("%nDigite o 2° número: ");
-            n2.setValor(scan.nextInt());
+
+            while (!scan.hasNextDouble()) {
+                System.out.println("Digite somente números: ");
+                scan.next();
+            }
+            n2.setValor(scan.nextDouble());
+
+
 
             double resultado = 0;
             switch(operador){
@@ -32,19 +43,24 @@ public class Calc{
                     resultado = n1.getValor() * n2.getValor();;
                     break;
                 case '/':
-                    resultado = n1.getValor() / n2.getValor();;
+                    if (n2.getValor() == 0) {
+                        System.out.println("Divisão por zero não é permitida!");
+                    } else {
+                        resultado = n1.getValor() / n2.getValor();
+                    }
                     break; 
                 default:
                     System.out.println("Operador inválido! Digite novamente: ");
-                    
+                    continue;
                 }
 
             System.out.printf("%.2f %c %.2f = %.2f%n", n1.getValor(), operador, n2.getValor(), resultado);
 
+            System.out.printf("%n--------------------");
+
             System.out.printf("%nDeseja fazer outro cálculo?(S | N): ");
             opc =scan.next();
 
-            System.out.printf("%n--------------------");
         } 
 
             System.out.printf("%nCalculadora finalizada.%nObrigado! :)");
